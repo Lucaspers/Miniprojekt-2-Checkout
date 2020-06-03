0a06a0078f4f5deb992fd8a40a8f358d9be3cc65
HumayunBA <humayun.bayani@gmail.com>
2020-06-03 12:24:25 +0200

    Checkout initial details

--- miniprojekt-checkout/src/pages/Checkout.tsx
+++ miniprojekt-checkout/src/pages/Checkout.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 const CartCheckOutPage = () => (
   <div className='CartCheckOutPage'>
-      Cart CheckOut Page
+     <h1>Shopping Details</h1>
+
+     <h1>Customer Details</h1>
+
+     <p>Före Namn</p>
+     <input type="text"/>
+     <p>Sista Namn</p>
+     <input type="text"/>
+     <p>Address</p>
+     <input type="text"/>
+     <p>Post Kod</p>
+     <input type="number"/>
+
+     <button>Slutför dit köp</button>
+
   </div>
 );
 
